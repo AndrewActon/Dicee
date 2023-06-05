d0d7bb03e35861a3a61d95cac551f8d65c9ae34d
Andrew Acton <andrew.acton.aa@gmail.com>
2023-06-05 16:21:19 -0600

    Interactive SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Andrew Acton on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
